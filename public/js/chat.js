855b7c6caa94de92f1c01ad3ffcc9a129da06d52
Rahul Shaw <rshaw@q3infotech.net>
2019-08-28 18:30:39 +0530

    Code to test the client server event

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,13 @@
-io();
+const socket = io();
+
+// socket.on('countUpdated', (count) => {
+//     console.log('The count has been updated:', count);
+// });
+
+// document.querySelector('#increment').addEventListener('click', () => {
+//     socket.emit('increment');
+//     console.log('clicked');
+// });
+socket.on('message', (message) => {
+    console.log(message);
+})
